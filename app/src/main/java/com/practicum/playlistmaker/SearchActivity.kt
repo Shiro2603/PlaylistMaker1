dbec74a50ad0d6f33802d5fe26fa19f65668bec3
Tima <tsuahnov26@gmail.com>
2024-06-22 16:59:50 +0500

    Сделал activity_seach, добавил логу кнопки удалит

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,6 +1,12 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +14,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SearchActivity : AppCompatActivity() {
+    var saveSearchText = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,5 +29,63 @@ class SearchActivity : AppCompatActivity() {
         buttonArrowBack.setOnClickListener{
             finish()
         }
+
+        val inputTextSearch = findViewById<EditText>(R.id.search_edit_text)
+        inputTextSearch.setSelectAllOnFocus(true)
+        val clearButton = findViewById<ImageView>(R.id.search_close_button)
+
+
+        clearButton.setOnClickListener{
+            inputTextSearch.setText("")
+            hideKeyboard(this, inputTextSearch)
+
+        }
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                saveSearchText = s.toString()
+                clearButton.visibility = clearButtonVisibility(s)
+
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        }
+
+
+        inputTextSearch.addTextChangedListener(textWatcher)
+        savedInstanceState?.let {
+            saveSearchText = it.getString("SearchText", "")
+            inputTextSearch.setText(saveSearchText)
+        }
+
+
+
     }
+   override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("SearchText", saveSearchText)
+    }
+
+
+    private fun hideKeyboard(context: Context, view: View) {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    fun clearButtonVisibility(s: CharSequence?):Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
 }
