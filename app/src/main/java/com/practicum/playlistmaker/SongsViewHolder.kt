65a81457d06d6b2d40f570d74a09032befa0c157
Tima <tsuahnov26@gmail.com>
2024-07-09 11:24:34 +0500

    Внес изменения согласно замечанию в PR, добавил placeholder согласно макету, изменил отображение картинок в светлой теме, вынес функцию dpToPx в отдельный файл

--- app/src/main/java/com/practicum/playlistmaker/SongsViewHolder.kt
+++ app/src/main/java/com/practicum/playlistmaker/SongsViewHolder.kt
@@ -9,6 +9,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.practicum.playlistmaker.Until.dpToPx
 
 class SongsViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
     private val songPicture : ImageView = itemView.findViewById(R.id.iv_song_picture)
@@ -19,7 +20,7 @@ class SongsViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
     fun bind(song: Track) {
         Glide.with(itemView.context)
             .load(song.artworkUrl100)
-            .placeholder(R.drawable.ic_close)
+            .placeholder(R.drawable.pc_placeholder)
             .centerCrop()
             .transform(RoundedCorners(dpToPx(2f, itemView.context)))
             .into(songPicture)
@@ -30,11 +31,6 @@ class SongsViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
         songTime.text = song.trackTime
     }
 
-    fun dpToPx(dp: Float, context: Context): Int {
-        return TypedValue.applyDimension(
-            TypedValue.COMPLEX_UNIT_DIP,
-            dp,
-            context.resources.displayMetrics).toInt()
-    }
+
 }
 
